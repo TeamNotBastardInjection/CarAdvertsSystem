c11e463041a43b3c1af74be4f798e30a3277cd36
Azonic89 <ooze89@yahoo.com>
2017-02-10 18:22:38 +0200

    UnitOfWork + Bytes2validation added

---
+++ CarAdvertsSystem/CarAdvertsSystem.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using CarAdvertsSystem.Data.Contracts;
+
+using Bytes2you.Validation;
+
+
+namespace CarAdvertsSystem.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ICarAdvertsSystemDbContext context;
+
+        public UnitOfWork(ICarAdvertsSystemDbContext context)
+        {
+            Guard.WhenArgument(context, "Db context is null!").IsNull().Throw();
+
+            this.context = context;
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
