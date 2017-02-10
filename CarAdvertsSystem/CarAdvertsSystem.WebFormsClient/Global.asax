f314308ff0e9f954c4da41cfaeac6dd02d69419d
Azonic89 <ooze89@yahoo.com>
2017-02-10 17:28:54 +0200

    Initial Architectural stuff

---
+++ CarAdvertsSystem/CarAdvertsSystem.WebFormsClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarAdvertsSystem.WebFormsClient.Global" Language="C#" %>
---
+++ CarAdvertsSystem/CarAdvertsSystem.WebFormsClient/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarAdvertsSystem.WebFormsClient.Startup))]
+namespace CarAdvertsSystem.WebFormsClient
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
