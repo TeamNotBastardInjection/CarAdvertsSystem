415b0cb91156c61c50264931ca52f5e6db069e6b
Azonic89 <ooze89@yahoo.com>
2017-02-11 21:24:10 +0200

    Added Data Bindings in Ninject WebClient to models

---
+++ CarAdvertsSystem/CarAdvertsSystem.WebFormsClient/App_Start/NinjectBindings/DataBindings.cs
@@ -0,0 +1,19 @@
+﻿using CarAdvertsSystem.Data.Contracts;
+using CarAdvertsSystem.Data.Repositories;
+using CarAdvertsSystem.Data.UnitOfWork;
+using Ninject.Modules;
+using Ninject.Extensions.Conventions;
+
+namespace CarAdvertsSystem.WebFormsClient.App_Start.NinjectBindings
+{
+    public class DataBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind(x => x.From("CarAdvertsSystem.Data.Models").SelectAllClasses().BindDefaultInterfaces());
+            this.Bind(typeof(IRepository<>)).To(typeof(GenericRepository<>));
+            this.Bind<ICarAdvertsSystemDbContext>().To<ICarAdvertsSystemDbContext>().InSingletonScope();
+            this.Bind<IUnitOfWork>().To<UnitOfWork>();
+        }
+    }
+}
