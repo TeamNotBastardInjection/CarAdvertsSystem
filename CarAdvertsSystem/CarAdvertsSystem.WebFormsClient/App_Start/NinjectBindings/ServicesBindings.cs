97e895279371b0b9b56f3284daf85112ed313373
Azonic89 <ooze89@yahoo.com>
2017-02-12 16:09:14 +0200

    Initial Services bindings Ninjectoyolo

---
+++ CarAdvertsSystem/CarAdvertsSystem.WebFormsClient/App_Start/NinjectBindings/ServicesBindings.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+using Ninject.Extensions.Conventions;
+
+namespace CarAdvertsSystem.WebFormsClient.App_Start.NinjectBindings
+{
+    public class ServicesBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind(x => x.From("CarAdvertsSystem.Data.Services").SelectAllClasses().BindDefaultInterfaces());
+        }
+    }
+}
