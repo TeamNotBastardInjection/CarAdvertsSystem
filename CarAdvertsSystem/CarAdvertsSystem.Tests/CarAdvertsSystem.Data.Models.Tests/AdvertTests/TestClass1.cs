5f5b72d15d69b0ade5111514c40c80dd49bc374f
Azonic89 <ooze89@yahoo.com>
2017-02-12 16:31:25 +0200

    Fixed a framework reference Error

---
+++ CarAdvertsSystem/CarAdvertsSystem.Tests/CarAdvertsSystem.Data.Models.Tests/AdvertTests/TestClass1.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using CarAdvertsSystem.Data.Models;
+using Castle.Components.DictionaryAdapter;
+using NUnit.Framework;
+
+namespace CarAdvertsSystem.Tests.CarAdvertsSystem.Data.Models.Tests.AdvertTests
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        [Test]
+        public void TestMethod()
+        {
+            
+        }
+    }
+}
