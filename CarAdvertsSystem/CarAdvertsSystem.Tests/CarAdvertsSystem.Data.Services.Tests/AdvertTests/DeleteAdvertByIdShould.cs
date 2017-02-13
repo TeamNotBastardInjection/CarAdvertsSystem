acd6635f89fa946b98c432fb132dede7f74a1b39
Azonic89 <ooze89@yahoo.com>
2017-02-13 12:28:31 +0200

    Added more Services tests

---
+++ CarAdvertsSystem/CarAdvertsSystem.Tests/CarAdvertsSystem.Data.Services.Tests/AdvertTests/DeleteAdvertByIdShould.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using CarAdvertsSystem.Data.Contracts;
+using CarAdvertsSystem.Data.Models;
+using CarAdvertsSystem.Data.Services;
+
+using Moq;
+using NUnit.Framework;
+
+namespace CarAdvertsSystem.Tests.CarAdvertsSystem.Data.Services.Tests.AdvertTests
+{
+    [TestFixture]
+    public class DeleteAdvertByIdShould
+    {
+        [Test]
+        public void DeleteAdvertById_Should_BeCalled_IfAdvertToDeleteIsValid()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advertToDeleteWithId = new Mock<Advert>();
+            advertService.AddAdvert(advertToDeleteWithId.Object);
+            advertService.DeleteAdvertById(advertToDeleteWithId.Object.Id);
+
+            mockedRepository.Verify(rep => rep.Delete(advertToDeleteWithId.Object.Id));
+        }
+
+        [Test]
+        public void DeleteAdvertById_Should_BeCalledOnce_IfAdvertToDeleteIsValid()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advertToDeleteWithId = new Mock<Advert>();
+            advertService.AddAdvert(advertToDeleteWithId.Object);
+            advertService.DeleteAdvertById(advertToDeleteWithId.Object.Id);
+
+            mockedRepository.Verify(rep => rep.Delete(advertToDeleteWithId.Object.Id), Times.Once);
+
+        }
+
+        [Test]
+        public void DeleteAdvertById_Should_NotDeleteAdvert_IfItIsNotAddedInTheFirstPlace()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advertToDeleteWithId = new Mock<Advert>();
+
+            mockedRepository.Verify(rep => rep.Delete(advertToDeleteWithId.Object.Id), Times.Never);
+        }
+
+        [Test]
+        public void DeleteAdvertById_Should_CallSaveChanges_IfParamsAreValid()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advertWithId = new Mock<Advert>();
+            advertService.AddAdvert(advertWithId.Object);
+            advertService.DeleteAdvertById(advertWithId.Object.Id);
+
+            mockedUnitOfWork.Verify(u => u.SaveChanges(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void DeleteAdvertById_Should_ThrowNullReferenceException_IfAdvertIsNull()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            Mock<Advert> advertWithId = null;
+
+            Assert.Throws<NullReferenceException>(() => advertService.DeleteAdvertById(advertWithId.Object.Id));
+        }
+    }
+}
---
+++ CarAdvertsSystem/CarAdvertsSystem.Tests/CarAdvertsSystem.Data.Services.Tests/AdvertTests/DeleteAdvertShould.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using CarAdvertsSystem.Data.Contracts;
+using CarAdvertsSystem.Data.Models;
+using CarAdvertsSystem.Data.Services;
+
+using Moq;
+using NUnit.Framework;
+
+namespace CarAdvertsSystem.Tests.CarAdvertsSystem.Data.Services.Tests.AdvertTests
+{
+    [TestFixture]
+    public class DeleteAdvertShould
+    {
+        [Test]
+        public void DeleteAdvert_Should_BeCalled_IfAdvertIsValid()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advertToAddAndDelete = new Mock<Advert>();
+            advertService.AddAdvert(advertToAddAndDelete.Object);
+            advertService.DeleteAdvert(advertToAddAndDelete.Object);
+
+            mockedRepository.Verify(rep => rep.Delete(advertToAddAndDelete.Object));
+        }
+
+        [Test]
+        public void DeleteAdvert_ShouldThrowNullReferenceException_IfAdvertIsNull()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            Mock<Advert> advertAsNull = null;
+
+            Assert.Throws<NullReferenceException>(() => advertService.DeleteAdvert(advertAsNull.Object));
+        }
+
+        [Test]
+        public void DeleteAdvert_Should_NotDeleteAdvertThatIsNeverAddedInTheFirstPlace()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advertThatIsNotAdded = new Mock<Advert>();
+
+            mockedRepository.Verify(rep => rep.Delete(advertThatIsNotAdded.Object), Times.Never);
+        }
+
+        [Test]
+        public void DeleteAdvert_Should_CallSaveChangesTwoTimes_IfAdvertIsValid()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advert = new Mock<Advert>();
+            advertService.AddAdvert(advert.Object);
+            advertService.DeleteAdvert(advert.Object);
+
+            mockedUnitOfWork.Verify(u => u.SaveChanges(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void DeleteAdvert_Should_NotCallSaveChanges_IfAdvertIsNotDelete()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advertThatIsNotAdded = new Mock<Advert>();
+
+            mockedUnitOfWork.Verify(u => u.SaveChanges(), Times.Never);
+        }
+
+        [Test]
+        public void DeleteAdvert_Should_BeCalledOnce_IfParamsAreValid()
+        {
+            var mockedRepository = new Mock<IRepository<Advert>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var advertService = new AdvertServices(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            var advert = new Mock<Advert>();
+            advertService.AddAdvert(advert.Object);
+            advertService.DeleteAdvert(advert.Object);
+
+            mockedRepository.Verify(rep => rep.Delete(It.IsAny<Advert>()), Times.Once);
+        }
+    }
+}
