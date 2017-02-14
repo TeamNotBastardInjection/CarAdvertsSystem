9d754bdf1ba1431b7ef88a2e03142aeb67ebe099
Azonic89 <ooze89@yahoo.com>
2017-02-14 15:17:01 +0200

    Ninject Services Bindings Fix

--- CarAdvertsSystem/CarAdvertsSystem.WebFormsClient/App_Start/NinjectBindings/DataBindings.cs
+++ CarAdvertsSystem/CarAdvertsSystem.WebFormsClient/App_Start/NinjectBindings/DataBindings.cs
@@ -1,6 +1,8 @@
-﻿using CarAdvertsSystem.Data.Contracts;
+﻿using CarAdvertsSystem.Data;
+using CarAdvertsSystem.Data.Contracts;
 using CarAdvertsSystem.Data.Repositories;
 using CarAdvertsSystem.Data.UnitOfWork;
+
 using Ninject.Modules;
 using Ninject.Extensions.Conventions;
 
@@ -12,7 +14,7 @@ namespace CarAdvertsSystem.WebFormsClient.App_Start.NinjectBindings
         {
             this.Bind(x => x.From("CarAdvertsSystem.Data.Models").SelectAllClasses().BindDefaultInterfaces());
             this.Bind(typeof(IRepository<>)).To(typeof(GenericRepository<>));
-            this.Bind<ICarAdvertsSystemDbContext>().To<ICarAdvertsSystemDbContext>().InSingletonScope();
+            this.Bind<ICarAdvertsSystemDbContext>().To<CarAdvertsSystemDbContext>().InSingletonScope();
             this.Bind<IUnitOfWork>().To<UnitOfWork>();
         }
     }
