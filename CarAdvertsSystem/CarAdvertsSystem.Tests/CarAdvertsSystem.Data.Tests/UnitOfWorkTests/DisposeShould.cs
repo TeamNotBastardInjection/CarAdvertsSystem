3426150d863c07f8f5c4b11cd2e3227a36487157
Azonic89 <ooze89@yahoo.com>
2017-02-20 14:20:44 +0200

    UnitOfWork 100% Coverage

---
+++ CarAdvertsSystem/CarAdvertsSystem.Tests/CarAdvertsSystem.Data.Tests/UnitOfWorkTests/DisposeShould.cs
@@ -0,0 +1,23 @@
+﻿using CarAdvertsSystem.Data.Contracts;
+using CarAdvertsSystem.Data.UnitOfWork;
+
+using NUnit.Framework;
+using Moq;
+
+namespace CarAdvertsSystem.Tests.CarAdvertsSystem.Data.Tests.UnitOfWorkTests
+{
+    [TestFixture]
+    public class DisposeShould
+    {
+        [Test]
+        public void Dispose_Should_BeCalledNever()
+        {
+            var dbContext = new Mock<ICarAdvertsSystemDbContext>();
+            var unitOfWork = new UnitOfWork(dbContext.Object);
+
+            unitOfWork.Dispose();
+
+            dbContext.Verify(u => u.Dispose(), Times.Never);
+        }
+    }
+}
---
+++ CarAdvertsSystem/CarAdvertsSystem.Tests/CarAdvertsSystem.Data.Tests/UnitOfWorkTests/SaveChangesShould.cs
@@ -0,0 +1,22 @@
+﻿using CarAdvertsSystem.Data.Contracts;
+using CarAdvertsSystem.Data.UnitOfWork;
+using Moq;
+using NUnit.Framework;
+
+namespace CarAdvertsSystem.Tests.CarAdvertsSystem.Data.Tests.UnitOfWorkTests
+{
+    [TestFixture]
+    public class SaveChangesShould
+    {
+        [Test]
+        public void SaveChanges_Should_CallSaveChangesOnce()
+        {
+            var dbContext = new Mock<ICarAdvertsSystemDbContext>();
+            var unitOfWork = new UnitOfWork(dbContext.Object);
+
+            unitOfWork.SaveChanges();
+
+            dbContext.Verify(u => u.SaveChanges(), Times.Once);
+        }
+    }
+}
